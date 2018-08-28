d24c1a370a8d94beaa89f9c0dbd108728dc90d86
malasik <kenvilar@gmail.com>
2018-08-28 21:12:59 +0800

    Build a command from the user input and expand the third argument of what user wrote

--- main.go
+++ main.go
@@ -32,5 +32,10 @@ func parent() {
 }
 
 func child() {
-	fmt.Printf("running: %v\n", os.Args[1:])
+	fmt.Printf("running: %v\n", os.Args[2:])
+	
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 }
